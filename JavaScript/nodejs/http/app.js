4228634c5271ecdfd1bd8a0f7bf559b5d31738c9
Jonesh Shrestha <contact@joneshshrestha.com>
2021-06-02 16:17:04 +0545

    update

--- JavaScript/nodejs/http/app.js
+++ JavaScript/nodejs/http/app.js
@@ -1,6 +1,9 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  //   res.writeHead(200, { "content-type": "text/html" });
+  //   fs.createReadStream("index.html").pipe(res);
   if (req.url === "/") {
     res.write("Hello World!");
     res.end();
@@ -13,6 +16,6 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000);
+server.listen(process.env.PORT || 3000);
 
 console.log("Listening on port 3000");

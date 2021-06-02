da8de3d302509acbf8fbd040f4adff9775882270
Jonesh Shrestha <contact@joneshshrestha.com>
2021-06-02 16:11:19 +0545

    udpate nodejs

---
+++ JavaScript/nodejs/express/app.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+const staticPath = path.join(__dirname);
+
+// Express Middleware
+app.use(express.static(staticPath));
+
+// app.get("/", (req, res) => {
+//   res.send("hello world!");
+// });
+
+app.get("/test", (req, res) => {
+  res.send("TEST");
+});
+
+app.use((req, res) => {
+  res.status(404).send("<h1> 404 ERROR </h1>");
+});
+
+app.listen(8000, () => {
+  console.log("Listening at port 8000");
+});

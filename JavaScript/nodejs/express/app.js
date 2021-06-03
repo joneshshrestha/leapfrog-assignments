9d1ce6445c87b205b2ea0b31f368846a009fe080
Jonesh Shrestha <contact@joneshshrestha.com>
2021-06-03 07:41:41 +0545

    express update

--- JavaScript/nodejs/express/app.js
+++ JavaScript/nodejs/express/app.js
@@ -1,23 +1,23 @@
-const path = require("path");
 const express = require("express");
 const app = express();
+const router = express.Router();
+const path = require("path");
 
 const staticPath = path.join(__dirname);
 
-// Express Middleware
-app.use(express.static(staticPath));
-
-// app.get("/", (req, res) => {
-//   res.send("hello world!");
-// });
-
-app.get("/test", (req, res) => {
-  res.send("TEST");
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
-app.use((req, res) => {
-  res.status(404).send("<h1> 404 ERROR </h1>");
+router.get("/about", function (req, res) {
+  res.sendFile(path.join(__dirname + "/about.html"));
 });
+
+router.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/404.html"));
+});
+
+app.use("/", router);
 
 app.listen(process.env.PORT || 8000, () => {
   console.log("Listening at port 8000");
